40ae63eb0e1632862767defc253e6b76e00e9b69
Bharath Gopal <bharath.taurus2@gmail.com>
2019-02-20 23:27:49 +0530

    Added changes to external mongodb connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
-mongoose.connect('mongodb+srv://bharath_taurus2:thtnikgh1@cluster0-fxqym.mongodb.net/test?retryWrites=true',
+mongoose.connect('mongodb+srv://bharath_taurus2:thtnikgh1@cluster0-fxqym.mongodb.net/Todos?retryWrites=true',
 {useNewUrlParser: true});
 
 module.exports = {mongoose};
