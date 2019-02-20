f31c9b0e6b400a5c6c29bd52449ebfbf150a6cd4
Bharath Gopal <bharath.taurus2@gmail.com>
2019-02-20 23:01:38 +0530

    Setup app for external mongodb cluster with heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+// mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://bharath_taurus2:thtnikgh1@cluster0-fxqym.mongodb.net/test?retryWrites=true',
+{useNewUrlParser: true});
 
 module.exports = {mongoose};
